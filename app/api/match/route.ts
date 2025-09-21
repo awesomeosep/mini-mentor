7786f90fa69cdbf93b79a320e429497be779deff
Josh Pinto <joshpinto6@outlook.com>
2025-09-20 23:06:18 -0400

    :ambulance: Fix nonexistant UPDATE request type

--- app/api/match/route.ts
+++ app/api/match/route.ts
@@ -79,7 +79,7 @@ export async function GET() {
   }
 }
 
-export async function UPDATE(request: Request) {
+export async function PUT(request: Request) {
   try {
     const supabase = await createClient();
     const {
